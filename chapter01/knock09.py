8b4e2b87bf050a5e1d87a83860e0926ec1325ee1
kkooyyaa <keu989.uregina@gmail.com>
2019-04-22 17:32:06 +0900

    add

---
+++ chapter01/knock09.py
@@ -0,0 +1,26 @@
+import random
+
+sentence = input()
+
+##sentence = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind"
+
+words = sentence.split()
+count = 0
+
+for i in words:
+    if len(i) >4:
+        letterlist = list(i)
+        mid = letterlist[1:-1]
+        random.shuffle(mid)
+        i = letterlist[0]+ ''.join(mid) + letterlist[-1]
+        words[count] = i
+    count+=1
+
+print(" ".join(words))
+
+
+
+
+
+
+       
