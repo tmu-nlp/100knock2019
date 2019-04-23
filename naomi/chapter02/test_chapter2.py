2dfe80b6bdf6eb72305a8ffe45216e892122a621
NMGD <naomi@komachi.live>
2019-04-23 13:50:54 +0900

    add knock012 and its test code

--- naomi/chapter02/test_chapter2.py
+++ naomi/chapter02/test_chapter2.py
@@ -3,6 +3,7 @@ import os
 import filecmp
 from knock010 import count_file_lines_py, count_file_lines_unix
 from knock011 import convfile_tab2space, convfile_tab2space_unix
+from knock012 import extcolumn, extcolumn_unix
 
 class TestKnockChp2(unittest.TestCase):
     def test_knock010(self):
@@ -20,6 +21,16 @@ class TestKnockChp2(unittest.TestCase):
         print("compare "+conv_fname_py+" and "+conv_fname_unix)
         self.assertTrue(filecmp.cmp(conv_fname_py,conv_fname_unix,shallow=False))
         # self.assertTrue(filecmp.cmp(conv_fname_py,'py_tab2space_hightemp1.txt',shallow=False))
+
+    def test_knock012(self):
+        print("test knock012")
+        in_file='hightemp.txt'
+        extcolumn(0,in_file,'col1.txt')
+        extcolumn_unix(0,in_file,'col1_unix.txt')
+        extcolumn(1,in_file,'col2.txt')
+        extcolumn_unix(1,in_file,'col2_unix.txt')
+        self.assertTrue(filecmp.cmp('col1.txt','col1_unix.txt',shallow=False))
+        self.assertTrue(filecmp.cmp('col2.txt','col2_unix.txt',shallow=False))
 
 if __name__=='__main__':
     unittest.main()
