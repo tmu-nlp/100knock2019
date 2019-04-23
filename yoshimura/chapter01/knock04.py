936bc27f004da76a13952c4b0832b9be73d708ed
RyomaYoshimura <ryoma@RyomaYoshimuranoMacBook-ea.local>
2019-04-23 18:04:41 +0900

    change file name

--- yoshimura/chapter01/knock04.py
+++ yoshimura/chapter01/knock04.py
@@ -3,7 +3,7 @@ s = "Hi He Lied Because Boron Could Not Oxidize Fluorine. \
 
 d = {}
 for i, w in enumerate(s.split(), 1):
-    if w in [1, 5, 6, 7, 8, 9, 15, 16, 19]:
+    if i in [1, 5, 6, 7, 8, 9, 15, 16, 19]:
         d[w[0]] = i
     else:
         d[w[:2]] = i
