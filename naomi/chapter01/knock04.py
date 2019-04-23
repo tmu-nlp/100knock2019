4d9113be5a43c78e41ff4f650cd4460fa247abd9
NMGD <naomi@komachi.live>
2019-04-23 17:58:59 +0900

    modified files after review

--- naomi/chapter01/knock04.py
+++ naomi/chapter01/knock04.py
@@ -1,9 +1,9 @@
-str='Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
+string1='Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
 
-str=str.replace(","," ")
-str=str.replace("."," ")
+string1=string1.replace(","," ")
+string1=string1.replace("."," ")
 
-words=str.split()
+words=string1.split()
 
 placefor1=(1,5,6,7,8,9,15,16,19)
 dict = {}
@@ -13,7 +13,8 @@ for word in words:
     if key in placefor1:
         dict[word[0]] = key
     else:
-        dict[word[0]+word[1]] = key
+        dict[word[:2]] = key
+        # dict[word[0]+word[1]] = key
     key+=1
 
 print(dict)
