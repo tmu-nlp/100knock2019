37c1c78d021e96d9a0e831d57a4ebfd1d1093543
NMGD <naomi@komachi.live>
2019-04-25 09:26:15 +0900

    Refactored knock14

--- naomi/chapter02/knock14.py
+++ naomi/chapter02/knock14.py
@@ -1,7 +1,7 @@
 
 import subprocess
 
-def shownlines(n: int, in_fname: str, out_fname: str):
+def headfile(n: int, in_fname: str, out_fname: str):
     with open(in_fname) as fin,open(out_fname,'w') as fout:
         content=fin.readlines()
         for counter, l in enumerate(content):
@@ -12,7 +12,7 @@ def shownlines(n: int, in_fname: str, out_fname: str):
     return
 
 
-def shownlines_unix(n: int, in_fname: str, out_fname: str):
+def headfile_unix(n: int, in_fname: str, out_fname: str):
     with open(out_fname,'w') as fout:
         subprocess.run(["head","-n",str(n),in_fname],stdout=fout)
     return
