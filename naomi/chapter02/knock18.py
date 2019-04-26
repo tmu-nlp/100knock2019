9ce55b135a9229fb92707dfe629ac7a672a24535
NMGD <naomi@komachi.live>
2019-04-26 10:27:01 +0900

    Modified knock18 (Test: fail)

--- naomi/chapter02/knock18.py
+++ naomi/chapter02/knock18.py
@@ -6,30 +6,31 @@ from operator import itemgetter
 def sort_column(col: int, in_fname: str, out_fname: str):
     with open(in_fname) as fin,open(out_fname,'w') as fout:
         lines=fin.readlines()
-        locs=[]
-        
-        for line in lines:
-            locs.append(cityclass(line.split()[0],line.split()[1],line.split()[2],line.split()[3]))
+        cities=[]
 
-        # sortedlines=sorted(linelist,key=itemgetter(col-1))
-        sortedlocs=sorted(locs, key=lamda cityclass: cityclass.temp)
-                
-        # for line in sortedlines:
-        #         fout.write('\t'.join(line)+'\n')
+        for line in lines:
+            cities.append(line.split())
+
+        sortedcities=sorted(cities, key=lambda x:x[2], reverse=True)
+
+        for line in sortedcities:
+            print(line)
+            fout.write('\t'.join(line)+'\n')
     return
 
 
 def sort_column_unix(col: int, in_fname: str, out_fname: str):
+
     with open(out_fname,'w') as fout:
-        ps_cut = subprocess.Popen(['sort','--key',str(col-1),in_fname], stdout=fout)
+# https://unix.stackexchange.com/questions/104525/sort-based-on-the-third-column
+        # subprocess.Popen(['sort','-k',str(col-1),','+str(col-1),in_fname], stdout=fout)
+        subprocess.run(['sort','-r','-k3',in_fname], stdout=fout)
+
     return
 
+if __name__=='__main__':
+    main()
 
-class cityclass:
-    def __init__(self, pref, city, temp, date):
-        self.pref = pref
-        self.city = city
-        self.temp = temp
-        self.date = date
-    def __repr__(self):
-        return repr((self.pref, self.city, self.temp, self.date))
+def main():
+
+    return
