8f7f14612d76ce9d97ee99980ea8535b6550ed09
NMGD <naomi@komachi.live>
2019-04-29 17:30:27 +0900

    add knock21 and its test code (pass)

--- naomi/chapter03/test_chapter3.py
+++ naomi/chapter03/test_chapter3.py
@@ -1,12 +1,14 @@
 # python -m unittest discover
 import unittest
 import filecmp
+import re
 from knock20 import jsons2dict
+from knock21 import extcategories
 
 class TestKnockChp3(unittest.TestCase):
-    def test_jsons2dict(self):
+    def test20_jsons2dict(self):
         print('test_jsons2dict')
-        in_file='test.json'
+        in_file='test1.json'
 
         refdict = { "text": "#転送 [[アイルランド共和国]]",\
                     "title": "アイルランド"}
@@ -15,6 +17,24 @@ class TestKnockChp3(unittest.TestCase):
             mydict = jsons2dict(fin,'アイルランド')
         
         self.assertEqual(mydict,refdict)
+    
+    def test21_extcategories(self):
+        print('test21_extcategories')
+        in_file ='test1.json'
+
+        with open(in_file) as fin:
+            mydict = jsons2dict(fin,'南オセチア')
+            refcategories=[]
+            refcategories.append('[[Category:南オセチア|*]]')
+            refcategories.append('[[Category:グルジアの地理]]')
+            refcategories.append('[[Category:カフカス]]')
+            refcategories.append('[[Category:オセット]]')
+
+            mycategories = extcategories(mydict['text'])
+
+
+            self.assertEqual(refcategories,mycategories)
+
 
 if __name__=='__main__':
     unittest.main()
