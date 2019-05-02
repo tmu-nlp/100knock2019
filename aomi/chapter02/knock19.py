d5a8cbd521a5b18eb24a739f899d44bec6f4aaad
aomik <aomi@koyamaaominoMacBook-puro.local>
2019-05-03 08:39:10 +0900

    knock19

---
+++ aomi/chapter02/knock19.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+my_dict = defaultdict(lambda: 0)
+
+with open('hightemp.txt', 'r') as hightemp_file:
+    for line in hightemp_file:
+        line = line.split('\t')
+        my_dict[line[0]] += 1
+my_dict = sorted(my_dict.items(), key = lambda x: -x[1])
+for key, value in my_dict:
+    print(key + ":" + str(value))
