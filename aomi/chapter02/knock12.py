c2425083714fd1e400fde99a541e7eef5b06f994
aomik <aomi@koyamaaominoMacBook-puro.local>
2019-05-03 04:15:11 +0900

    knock12

---
+++ aomi/chapter02/knock12.py
@@ -0,0 +1,7 @@
+import sys
+
+with open(sys.argv[1], 'r') as my_file, open('col1.txt', 'w') as col1_file, open('col2.txt', 'w') as col2_file:
+    for line in my_file:
+        line = line.split('\t')
+        print(line[0], file = col1_file)
+        print(line[1], file = col2_file)
