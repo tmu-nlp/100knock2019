db3b98b72b304994ac7c9043efd93abc976f0479
aomik <aomi@koyamaaominoMacBook-puro.local>
2019-05-04 16:46:31 +0900

    knock23

--- aomi/chapter03/knock23.py
+++ aomi/chapter03/knock23.py
@@ -1,10 +1,9 @@
 import re
 
 with open('jawiki-UK.txt') as UK_file:
-    pattern = '(=+)(.*)=+'
+    pattern = '(=+)(.+?)(=+)'
     for line in UK_file:
         result = re.match(pattern, line)
         if result:
             cnt = len(result.group(1))
-            print(result.group(), end = '')
-            print(cnt - 1)
+            print( " セクション名:" + result.group(2) + " レベル:" + str(cnt - 1))
