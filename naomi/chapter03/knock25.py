6cc517c58d65ccd1a7fa2a88023c2c90206dcb8e
NMGD <naomi@komachi.live>
2019-05-09 15:23:32 +0900

    add test and code for 25 (pass)

---
+++ naomi/chapter03/knock25.py
@@ -0,0 +1,39 @@
+import re
+import gzip
+from collections import defaultdict
+from knock20 import jsons2dict
+
+
+def exttemplate(text: str) -> dict:
+
+    td = defaultdict(lambda: 0)
+
+    entries = re.split('\n+', text)
+
+    p = re.compile(r"""
+    (^\| (?P<key>.*?)
+    \s=\s
+    (?P<value>.*$))
+    """, re.VERBOSE)
+
+    for entry in entries:
+        m = p.match(entry)
+        if m:
+            key = m.group('key').strip('|')
+            key = key.strip()
+            value = m.group('value').strip()
+            print('key: {0}, value: {1}'.format(key, value))
+            td[key] = value
+    return td
+
+
+def main():
+    with gzip.open('jawiki-country.json.gz', 'rt', encoding='utf-8') as fs:
+
+        data = jsons2dict(fs, 'イギリス')
+        template = exttemplate(data['text'])
+    return
+
+
+if __name__ == '__main__':
+    main()
