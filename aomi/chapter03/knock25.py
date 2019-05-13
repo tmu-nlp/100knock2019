a03f17ce61fd323e2eedf719e4cfa685760e35f8
aomik <aomi@koyamaaominoMacBook-puro.local>
2019-05-14 01:01:11 +0900

    knock

--- aomi/chapter03/knock25.py
+++ aomi/chapter03/knock25.py
@@ -2,7 +2,7 @@ import re
 import json
 import gzip
 
-pattern = re.compile(r'^\|(.+?)\s=\s(.+?)(\n)', re.MULTILINE + re.DOTALL)
+pattern = re.compile(r'^\|(.+?)\s=\s(.+?)\n', re.MULTILINE + re.DOTALL)
 
 UK_dict  = {}
 
@@ -10,8 +10,8 @@ with gzip.open('jawiki-country.json.gz', 'r') as jawiki_country_file:
     for line in jawiki_country_file:
         line = json.loads(line)
         if line['title'] == 'イギリス':
-            for ans in pattern.finditer(line['text']):
-                UK_dict[ans.group(1)] = ans.group(2)
+            for ans in re.findall(pattern, line['text']):
+                UK_dict[ans[0]] = ans[1]
 
 for key, value in UK_dict.items():
     print(key + ":" + value)
--- aomi/chapter03/knock27.py
+++ aomi/chapter03/knock27.py
@@ -9,7 +9,9 @@ pattern = re.compile(r'^\|(.+?)\s=\s(.+?)(\n)', re.MULTILINE + re.DOTALL)
 pattern2 = re.compile(r'\'{2,5}', re.MULTILINE + re.DOTALL)
 
 #内部リンクの除去
-pattern3 = re.compile(r'(\[\[)|(\]\])|(\|)|(\]\])', re.MULTILINE + re.DOTALL)
+#[[記事名]]->記事名, [[記事名|表示文字]]->表示文字, [[記事名#節名|表示文字]]->表示文字
+pattern3 = re.compile(r'\[{2}(?:[^|]*?\|)*?([^|]*?)\]{2}', re.MULTILINE + re.DOTALL)
+
 
 UK_dict  = {}
 
@@ -19,7 +21,7 @@ with gzip.open('jawiki-country.json.gz', 'r') as jawiki_country_file:
         if line['title'] == 'イギリス':
             for ans in pattern.finditer(line['text']):
                 ans2 = pattern2.sub("", ans.group(2))
-                ans2 = pattern3.sub("", ans2)
+                ans2 = pattern3.sub(r'\1', ans2)
                 UK_dict[ans.group(1)] = ans2
 
 for key, value in UK_dict.items():
