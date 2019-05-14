d5612da9e099dbcb57ea77ff8f42d8f10d501163
wkwkgg <yujin@komachi.live>
2019-05-14 09:00:37 +0900

    add chapter03

--- takahashi/chapter03/knock29.py
+++ takahashi/chapter03/knock29.py
@@ -2,27 +2,30 @@
 # テンプレートの内容を利用し，国旗画像のURLを取得せよ
 
 from knock20 import get_country_data
-from knock25 import extract_basic_info, extract_name_and_value
-import requests
+from knock25 import get_template
+import urllib3
+import json
 import re
 
-def get_national_flag_url(title: str) -> str:
+
+def get_national_flag_url(target: str) -> str:
+    title = re.sub(" ", "_", get_template(target)["国旗画像"])
     url = "https://en.wikipedia.org/w/api.php"
     params = {
-        "action" : "query",
-        "format" : "json",
-        "prop"   : "imageinfo",
-        "iiprop" : "url",
-        "titles" : "File:{}".format(title.replace(" ", "_"))
+        "action": "query",
+        "format": "json",
+        "prop": "imageinfo",
+        "iiprop": "url",
+        "titles": f"File:{title}",
     }
-    session = requests.Session()
-    req = session.get(url=url, params=params)
-    res = req.json()
-    # res の値から url への path を調べる
+
+    http = urllib3.PoolManager()
+    req = http.request("GET", url, fields=params)
+    res = json.loads(req.data.decode("utf-8"))
+
     return res["query"]["pages"]["23473560"]["imageinfo"][0]["url"]
+
 
 if __name__ == "__main__":
     target = get_country_data("イギリス")
-    basic_info = extract_basic_info(target)
-    title = extract_name_and_value(basic_info)["国旗画像"]
-    print(get_national_flag_url(title))
+    print(get_national_flag_url(target))
