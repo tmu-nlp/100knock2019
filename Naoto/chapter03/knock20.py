b2af820f347a00f37b4259a50cf21d0ec62de3e9
Naoto Nakazawa <naoto_nakazawa@NaotonoMacBook-Pro.local>
2019-05-14 16:19:25 +0900

    c

--- Naoto/chapter03/knock20.py
+++ Naoto/chapter03/knock20.py
@@ -1,14 +1,10 @@
 import json
 
 
-count = 0
 with open("jawiki-country.json") as f, open("jawiki-イギリス.json", mode="w") as イギリス:
     article_json = f.readline()
-    if count == 0:
-        # print(type(article_json))
-        count += 1
     while article_json:
         article_dict = json.loads(article_json)
-        if article_dict["title"] == u"イギリス":
+        if article_dict["title"] == "イギリス":
             イギリス.write(article_dict["text"])
         article_json = f.readline()
