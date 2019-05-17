645a41a4e3ac169b7f09abebaa4f77572c41adf6
reo hirao <reo@reonoMacBook-Air.local>
2019-05-17 12:52:20 +0900

    update knock30-39

--- hirao/chapter04/knock32.py
+++ hirao/chapter04/knock32.py
@@ -1,11 +1,14 @@
 from knock30 import load_txt
-SIZE = 15
 
-word_list = load_txt("neko.txt.mecab")
+OUTPUT_SIZE = 15
+
+# 文ごとの形態素解析結果を受け取る
+sentence_list = load_txt("neko.txt.mecab")
 
 verb = []
-for i in range(len(word_list)):
-    word = word_list[i]
-    if word["pos"] == "動詞":
-        verb.append(word["base"])
-print(", ".join(verb[:SIZE]))
+for word_list in sentence_list:
+    for i in range(len(word_list)):
+        word = word_list[i]
+        if word["pos"] == "動詞":
+            verb.append(word["base"])
+print(", ".join(verb[:OUTPUT_SIZE]))
--- hirao/chapter04/knock34.py
+++ hirao/chapter04/knock34.py
@@ -1,11 +1,16 @@
 from knock30 import load_txt
-SIZE = 15
 
-word_list = load_txt("neko.txt.mecab")
+OUTPUT_SIZE = 15
+
+sentence_list = load_txt("neko.txt.mecab")
 
 no = []
 
-for i in range(len(word_list)-2):
-    if word_list[i]["pos"] == "名詞" and word_list[i+1]["surface"] == "の" and word_list[i+2]["pos"] == "名詞":
-        no.append(word_list[i]["surface"] + word_list[i+1]["surface"] + word_list[i+2]["surface"])
-print(", ".join(no[:SIZE]))
+for word_list in sentence_list:
+    # 名詞 + の + 名詞なので、3文字探索
+    for i in range(len(word_list)-2):
+        if word_list[i]["pos"] == "名詞" and \
+        word_list[i+1]["surface"] == "の" and \
+        word_list[i+2]["pos"] == "名詞":
+            no.append(word_list[i]["surface"] + word_list[i+1]["surface"] + word_list[i+2]["surface"])
+print(", ".join(no[:OUTPUT_SIZE]))
