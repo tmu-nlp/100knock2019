4d870039b67554b9e717ea30c697cda0f9a2b45e
wkwkgg <yujin@komachi.live>
2019-05-22 11:23:08 +0900

    add chapter04

--- takahashi/chapter04/knock38.py
+++ takahashi/chapter04/knock38.py
@@ -4,22 +4,26 @@
 from knock30 import load_morpheme_list
 from knock36 import get_word_frequency
 
-from typing import Dict, List, Tuple
+from typing import List, Dict, Tuple, Iterable
 from matplotlib import pyplot as plt
 from matplotlib.font_manager import FontProperties
 from os.path import expanduser
 
 # 日本語フォントの読み込み
-fp = FontProperties(fname = "/".join([expanduser("~"), "Library", "Fonts", "NotoSansCJKjp-DemiLight.otf"]))
+fp = FontProperties(
+    fname="/".join([expanduser("~"), "Library", "Fonts", "NotoSansCJKjp-DemiLight.otf"])
+)
 
-def histogram(freq: Dict[str, int]) -> None:
-    plt.hist(list(freq.values()), bins=20, range=(1,50))
+
+def main(freq: Iterable[Tuple[str, int]]) -> None:
+    values = [value for (_, value) in freq]
+    plt.hist(values, bins=50, range=(1, 50))
     plt.title("ヒストグラム", fontproperties=fp)
     plt.xlabel("出現頻度", fontproperties=fp)
     plt.ylabel("単語の種類数", fontproperties=fp)
     plt.show()
 
+
 if __name__ == "__main__":
     freq = get_word_frequency(load_morpheme_list())
-    histogram(freq)
-
+    main(freq)
--- takahashi/chapter04/knock39.py
+++ takahashi/chapter04/knock39.py
@@ -4,20 +4,22 @@
 from knock30 import load_morpheme_list
 from knock36 import get_word_frequency
 
-from typing import Dict, List, Tuple
+from typing import List, Dict, Tuple, Iterable
 from matplotlib import pyplot as plt
 from matplotlib.font_manager import FontProperties
 from os.path import expanduser
 
 # 日本語フォントの読み込み
-fp = FontProperties(fname = "/".join([expanduser("~"), "Library", "Fonts", "NotoSansCJKjp-DemiLight.otf"]))
+fp = FontProperties(
+    fname="/".join([expanduser("~"), "Library", "Fonts", "NotoSansCJKjp-DemiLight.otf"])
+)
 
-def zipf(freq: Dict[str, int]) -> None:
-    freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-    freq = [v for (k,v) in freq]
-    rank = range(1,len(freq)+1)
 
-    plt.scatter(rank, freq, s=1)
+def zipf(freq: Iterable[Tuple[str, int]]) -> None:
+    value = [v for _, v in freq]  # type: List[int]
+    rank = range(1, len(value) + 1)
+
+    plt.scatter(rank, value, s=1)
     plt.title("Zipfの法則", fontproperties=fp)
     plt.xlabel("単語の出現頻度順位", fontproperties=fp)
     plt.ylabel("単語の出現頻度", fontproperties=fp)
@@ -25,6 +27,6 @@ def zipf(freq: Dict[str, int]) -> None:
     plt.yscale("log")
     plt.show()
 
+
 if __name__ == "__main__":
-    freq = get_word_frequency(load_morpheme_list())
-    zipf(freq)
+    zipf(get_word_frequency(load_morpheme_list()))
