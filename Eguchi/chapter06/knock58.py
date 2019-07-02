1510212336349827deff74e8df48831708a8de48
kkooyyaa <keu989.uregina@gmail.com>
2019-07-02 15:12:15 +0900

    add

--- Eguchi/chapter06/knock58.py
+++ Eguchi/chapter06/knock58.py
@@ -1,18 +1,49 @@
+#Stanford Core NLPの係り受け解析の結果（collapsed-dependencies）に基づき，「主語 述語 目的語」の組をタブ区切り形式で出力せよ．
+# ただし，主語，述語，目的語の定義は以下を参考にせよ．
+#述語: nsubj関係とdobj関係の子（dependant）を持つ単語
+#主語: 述語からnsubj関係にある子（dependent）
+#目的語: 述語からdobj関係にある子（dependent）
 
-from graphviz import Digraph
+import re
+import xml.etree.ElementTree as ET
 
-# formatはpngを指定(他にはPDF, PNG, SVGなどが指定可)
-G = Digraph(format='png')
-G.attr('node', shape='circle')
+path = r"C:\Users\Koya\Documents\Lab\100knock2019\Eguchi\chapter06\nlp.txt"
+path2 = r"C:\Users\Koya\Documents\Lab\100knock2019\Eguchi\chapter06\nlp.txt.xml"
 
+def analyzer():
+    tree = ET.parse(path2)
+    root = tree.getroot()
 
+    predicates = {}
+    subjects = {}
+    objects = {}
 
-# 辺の追加
-G.edge("a", "b")
-G.edge("b", "a")
-G.edge("c", "a")
+    for sentence in root.iterfind("./document/sentences/sentence"): 
+        predicates = {}
+        subjects = {}
+        objects = {}       
 
-print(G)
+        for dep in sentence.iterfind('./dependencies[@type="collapsed-dependencies"]/dep'):
+            
+            dep_type = dep.get("type") 
 
-# binary_tree.pngで保存
-G.render('binary_tree')
+            if dep_type == "nsubj" or "dobj":
+
+                governor = dep.find('./governor')
+                idx = governor.get("idx")
+                predicates[idx] = governor.text
+
+                if dep_type == "nsubj":
+                    subjects[idx] = dep.find('./dependent').text
+
+                elif dep_type == "dobj":
+                    objects[idx] = dep.find('./dependent').text
+        
+        for idx, pre in sorted(predicates.items(), key = lambda x: x[0] ):
+            sub = subjects.get(idx)
+            obj = objects.get(idx)
+            
+            if (sub != None) and (obj != None) :
+                print("%s\t%s\t%s" %(sub, pre, obj))
+
+analyzer()
