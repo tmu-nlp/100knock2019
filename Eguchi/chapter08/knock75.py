a22710512f3a556938dcb503e4b7f3423c01dd2f
kkooyyaa <keu989.uregina@gmail.com>
2019-07-30 13:37:07 +0900

    add

--- Eguchi/chapter08/knock75.py
+++ Eguchi/chapter08/knock75.py
@@ -1,2 +1,16 @@
 #73で学習したロジスティック回帰モデルの中で，重みの高い素性トップ10と，
 # 重みの低い素性トップ10を確認せよ
+
+import joblib
+
+logr = joblib.load("logr_model")
+name = joblib.load("tfidf_name")
+
+sorted_logr = sorted(list(zip(logr.coef_.flatten(), name)))
+
+print("Worst 10")
+for weight, name in sorted_logr[:10]:
+    print(f"{weight:.5}\t{name}")
+print("\nBest 10")
+for weight, name in sorted_logr[-10:][::-1]:
+    print(f"{weight:.5}\t{name}")
